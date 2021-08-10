62f62306d8c8df428cc3fc5680a126b51e610079
Julia Pineda <Julia.Pineda@analog.com>
2021-08-10 10:56:48 +0800

    Get board from nebula board name
    Signed-off-by: Julia Pineda <Julia.Pineda@analog.com>

--- test/runHWTests.m
+++ test/runHWTests.m
@@ -2,15 +2,15 @@ function runHWTests(board)
 
 import matlab.unittest.TestRunner;
 import matlab.unittest.TestSuite;
-import matlab.unittest.plugins.TestReportPlugin;
 import matlab.unittest.plugins.XMLPlugin
-import matlab.unittest.plugins.DiagnosticsValidationPlugin
+import matlab.unittest.plugins.TestReportPlugin;
 import matlab.unittest.parameters.Parameter
 import matlab.unittest.plugins.ToUniqueFile;
 import matlab.unittest.plugins.TAPPlugin;
 import matlab.unittest.constraints.ContainsSubstring;
 import matlab.unittest.selectors.HasName;
 import matlab.unittest.selectors.HasProcedureName;
+import matlab.unittest.plugins.DiagnosticsValidationPlugin
 
 at = {'AD9361Tests','AD9363Tests','AD9364Tests'...
         'AD9371Tests','ADRV9009Tests','DAQ2Tests'};
@@ -20,12 +20,38 @@ if nargin == 0
     suite = testsuite(at);
 else
     try
-        if contains(board,'ad9361','IgnoreCase',true)
-            board = 'ad9361';
-        elseif contains(board,'ad9364','IgnoreCase',true)
-            board = 'ad9364';
+        switch board
+        case "pluto"
+            board = {'Pluto'};
+        case {"zynq-adrv9361-z7035-bob-cmos", ...
+                "socfpga_cyclone5_sockit_arradio", ...
+                "zynq-zed-adv7511-ad9361-fmcomms2-3", ...
+                "zynq-zc702-adv7511-ad9361-fmcomms2-3", ...
+                "zynq-zc706-adv7511-ad9361-fmcomms2-3", ...
+                "zynqmp-zcu102-rev10-ad9361-fmcomms2-3", ...
+                "zynq-adrv9361-z7035-fmc", ...
+                "zynq-adrv9361-z7035-box", ...
+                "zynq-adrv9361-z7035-bob"
+                "zync-zed-adv7511-ad9361-fmcomms5"}
+            board = {'AD9361'};
+        case {"zynq-zc702-adv7511-ad9364-fmcomms4", ...
+                "zynq-zc706-adv7511-ad9364-fmcomms4", ...
+                "zynqmp-zcu102-rev10-ad9364-fmcomms4", ...
+                "zynq-adrv9364-z7020-box", ...
+                "zynq-adrv9364-z7020-bob", ...
+                "zynq-adrv9364-z7020-bob-cmos", ...
+                "zynq-zed-adv7511-ad9364-fmcomms4"}
+            board = {'AD9364'};
+        case {"zynqmp-zcu102-rev10-adrv9002", ...
+                "zynq-zed-adv7511-adrv9002", ...
+                "zynq-zed-adv7511-adrv9002-vcmos", ...
+                "zynqmp-zcu102-rev10-adrv9002-vcmos"}
+            board = {'ADRV9002'};
+        case {"zynq-zc706-adv7511-fmcdaq2"}
+            board = {'DAQ2'}
         end
     end
+    
     boards = lower(board);
     suite = testsuite(at);
     suite = selectIf(suite,HasProcedureName(ContainsSubstring(boards,'IgnoringCase',true)));
